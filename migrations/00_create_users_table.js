92820bad3b001c3b491710023be1fea5eb2f2686
ag_dubs <ashley666ashley@gmail.com>
2015-08-05 15:24:28 -0400

    remove notnullable constraint from columns with defaults

--- migrations/00_create_users_table.js
+++ migrations/00_create_users_table.js
@@ -2,9 +2,9 @@ exports.up = function(knex) {
   return knex.schema.createTable('users', function(t) {
     t.bigIncrements('id').unique().notNullable().primary();
     t.string('username').unique().notNullable();
-    t.string('language').notNullable().defaultTo('en-US');
-    t.boolean('moderator').notNullable().defaultTo(false);
-    t.boolean('staff').notNullable().defaultTo(false);
+    t.string('language').defaultTo('en-US');
+    t.boolean('moderator').defaultTo(false);
+    t.boolean('staff').defaultTo(false);
 
     t.timestamp('deleted_at').defaultTo(null);
     t.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
